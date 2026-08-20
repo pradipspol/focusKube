9c955a87d14839ed4cb618f0a6ced709814f047c
Pradip Pol <pradipspol@gmail.com>
2026-08-20 22:29:28 +0530

    feat: update installer script and add new icon assets

--- desktop/main.js
+++ desktop/main.js
@@ -308,7 +308,7 @@ async function createWindow(url) {
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
-    icon: path.join(__dirname, 'assets/icons/app.png'),
+    icon: path.join(__dirname, 'assets/icons/app_150.png'),
     webPreferences: {
       nodeIntegration: false,
       contextIsolation: true,
